24e8cc33d744867f37058b8ef9af511ca18d27fe
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-23 13:32:30 +0200

    setting up for local storage

--- script.js
+++ script.js
@@ -37,9 +37,11 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
